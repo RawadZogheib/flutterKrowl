9b9c03c7e417fb39bcbaca6f2857d07530081ea7
zogheibRawad <rawadzogheib@gmail.com>
2021-11-09 08:32:31 +0200

    .

--- lib/page/videoConference.dart
+++ lib/page/videoConference.dart
@@ -7,12 +7,13 @@ import 'package:flutter_ion/flutter_ion.dart' as ion;
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:uuid/uuid.dart';
 
-class VideoConference  extends StatefulWidget {
-  const VideoConference ({Key? key, required this.title}) : super(key: key);
+
+class VideoConference extends StatefulWidget {
+  VideoConference({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  _videoConference createState() => _videoConference();
+  _VideoConferenceState createState() => _VideoConferenceState();
 }
 
 class Participant {
@@ -23,139 +24,138 @@ class Participant {
   RTCVideoRenderer renderer;
 }
 
+class _VideoConferenceState extends State<VideoConference> {
+  List<Participant> plist = <Participant>[];
 
-class _videoConference extends State<VideoConference > {
-List<Participant> plist = <Participant>[];
+  // ion.Signal? _signal;
+  String ServerURL = '';
+  ion.Client? _client;
+  ion.LocalStream? _localStream;
+  final String _uuid = Uuid().v4();
 
-// ion.Signal? _signal;
-String ServerURL = '';
-ion.Client? _client;
-ion.LocalStream? _localStream;
-final String _uuid = Uuid().v4();
-
-@override
-void initState() {
-  super.initState();
-  initSignal();
-}
-
-initSignal() {
-  if (kIsWeb) {
-    ServerURL = 'http://localhost:9090';
-    //ServerURL = 'http://147.78.45.13:9090';
-  } else {
-    //ServerURL = 'http://147.78.45.13:9090';
-    ServerURL = 'http://10.10.4.64:9090';
+  @override
+  void initState() {
+    super.initState();
+    initSignal();
   }
-}
 
-late ion.Signal _signal = ion.GRPCWebSignal(ServerURL);
-
-void pubSub() async {
-  log("serverurl " + ServerURL);
-  if (_client == null) {
-    // create new client
-    _client = await ion.Client.create(
-        sid: "test room", uid: _uuid, signal: _signal);
-
-    // peer ontrack
-    _client?.ontrack = (track, ion.RemoteStream remoteStream) async {
-      if (track.kind == 'video') {
-        print('ontrack: remote stream: ${remoteStream.stream}');
-        var renderer = RTCVideoRenderer();
-        await renderer.initialize();
-        renderer.srcObject = remoteStream.stream;
-        setState(() {
-          plist.add(
-              Participant('RemoteStream', renderer, remoteStream.stream));
-        });
-      }
-    };
-    String type;
-
-    //type = "Camera";
-    type = "shareScreen";
-
-    if(type == "Camera") {
-      //create get user camera stream
-      _localStream = await ion.LocalStream.getUserMedia(
-          constraints: ion.Constraints.defaults..simulcast = false);
-    }else if(type == "shareScreen"){
-      // create get user ShareScreen stream
-      _localStream = await ion.LocalStream.getDisplayMedia(
-          constraints: ion.Constraints.defaults..simulcast = false);
-    }else{
-      exit;
+  initSignal() {
+    if (kIsWeb) {
+      //ServerURL = 'http://localhost:9090';
+      ServerURL = 'http://147.78.45.13:9090';
+    } else {
+      ServerURL = 'http://147.78.45.13:9090';
+      //ServerURL = 'http://10.10.4.64:9090';
     }
-    // publish the stream
-    await _client?.publish(_localStream!);
-
-    var renderer = RTCVideoRenderer();
-    await renderer.initialize();
-    renderer.srcObject = _localStream?.stream;
-    setState(() {
-      plist.add(Participant("LocalStream", renderer, _localStream?.stream));
-    });
-  } else {
-    // unPublish and remove stream from video element
-    await _localStream?.stream.dispose();
-    _localStream = null;
-    _client?.close();
-    _client = null;
   }
-}
+
+  late ion.Signal _signal = ion.GRPCWebSignal(ServerURL);
+
+  void pubSub() async {
+    log("serverurl " + ServerURL);
+    if (_client == null) {
+      // create new client
+      _client = await ion.Client.create(
+          sid: "test room", uid: _uuid, signal: _signal);
+
+      // peer ontrack
+      _client?.ontrack = (track, ion.RemoteStream remoteStream) async {
+        if (track.kind == 'video') {
+          print('ontrack: remote stream: ${remoteStream.stream}');
+          var renderer = RTCVideoRenderer();
+          await renderer.initialize();
+          renderer.srcObject = remoteStream.stream;
+          setState(() {
+            plist.add(
+                Participant('RemoteStream', renderer, remoteStream.stream));
+          });
+        }
+      };
+      String type;
+
+      //type = "Camera";
+      type = "shareScreen";
+
+      if(type == "Camera") {
+        //create get user camera stream
+        _localStream = await ion.LocalStream.getUserMedia(
+            constraints: ion.Constraints.defaults..simulcast = false);
+      }else if(type == "shareScreen"){
+        // create get user ShareScreen stream
+        _localStream = await ion.LocalStream.getDisplayMedia(
+            constraints: ion.Constraints.defaults..simulcast = false);
+      }else{
+        exit;
+      }
+      // publish the stream
+      await _client?.publish(_localStream!);
+
+      var renderer = RTCVideoRenderer();
+      await renderer.initialize();
+      renderer.srcObject = _localStream?.stream;
+      setState(() {
+        plist.add(Participant("LocalStream", renderer, _localStream?.stream));
+      });
+    } else {
+      // unPublish and remove stream from video element
+      await _localStream?.stream.dispose();
+      _localStream = null;
+      _client?.close();
+      _client = null;
+    }
+  }
 
 
-Widget getItemView(Participant item) {
-  log("items: " + item.toString());
-  return Container(
-      padding: EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              '${item.title}:\n${item.stream!.id}',
-              style: TextStyle(fontSize: 14, color: Colors.black54),
+  Widget getItemView(Participant item) {
+    log("items: " + item.toString());
+    return Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '${item.title}:\n${item.stream!.id}',
+                style: TextStyle(fontSize: 14, color: Colors.black54),
+              ),
             ),
-          ),
-          Expanded(
-            child: RTCVideoView(item.renderer,
-                objectFit:
-                RTCVideoViewObjectFit.RTCVideoViewObjectFitContain),
-          ),
-        ],
-      ));
-}
+            Expanded(
+              child: RTCVideoView(item.renderer,
+                  objectFit:
+                  RTCVideoViewObjectFit.RTCVideoViewObjectFitContain),
+            ),
+          ],
+        ));
+  }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text(widget.title),
-    ),
-    body: Container(
-      padding: EdgeInsets.all(10.0),
-      child: GridView.builder(
-        shrinkWrap: true,
-        itemCount: plist.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 5.0,
-          crossAxisSpacing: 5.0,
-          childAspectRatio: 1.0,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return getItemView(plist[index]);
-        },
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: pubSub,
-      tooltip: 'Increment',
-      child: Icon(Icons.video_call),
-    ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: GridView.builder(
+          shrinkWrap: true,
+          itemCount: plist.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 5.0,
+            crossAxisSpacing: 5.0,
+            childAspectRatio: 1.0,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return getItemView(plist[index]);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: pubSub,
+        tooltip: 'Increment',
+        child: Icon(Icons.video_call),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
 
-  );
-}
+    );
+  }
 }
