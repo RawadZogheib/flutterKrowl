68b0c879a413f6fe7cd0ade13a14338f9d479102
Clarazeidan <clarazaeidan@gmail.com>
2022-01-03 10:19:48 +0200

    .

--- lib/page/Test.dart
+++ lib/page/Test.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_backend/Data/ContentView.dart';
 import 'package:flutter_app_backend/globals/globals.dart' as globals;
 import 'package:flutter_app_backend/widgets/Forum/AskQuestionButton.dart';
+import 'package:flutter_app_backend/widgets/Forum/CreatePostContainer.dart';
 import 'package:flutter_app_backend/widgets/Forum/QuestionContainer.dart';
 import 'package:flutter_app_backend/widgets/Forum/SearchBar.dart';
 import 'package:flutter_app_backend/widgets/Forum/Contributors.dart';
@@ -24,170 +25,14 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> with SingleTickerProviderStateMixin {
-  late TabController tabController;
-  List<ContentView> contentViews = [
-    ContentView(
-        tab: CustomTab(title: 'Library'),
-        content: Center(
-          child: Container(
-            color: Colors.black,
-            width: 90,
-            height: 100,
-          ),
-        )),
-    ContentView(
-        tab: CustomTab(title: 'Chats'),
-        content: Center(
-          child: Container(),
-        )),
-    ContentView(
-        tab: CustomTab(title: 'Forum'),
-        content: Center(
-          child: Container(),
-        )),
-    ContentView(
-        tab: CustomTab(title: 'Students'),
-        content: Center(
-          child: Container(),
-        )),
-    ContentView(
-        tab: CustomTab(title: 'Reminders'),
-        content: Center(
-          child: Container(),
-        )),
-  ];
+
 
   @override
-  void initState() {
-    super.initState();
-    tabController = TabController(length: contentViews.length, vsync: this);
-  }
-
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
     return Scaffold(
         backgroundColor: globals.white,
-        body: Responsive(
-          mobile: SingleChildScrollView(
-            reverse: true,
-            child: Container(
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CreateTable(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Wrap(
-                                children: [
-                                  Column(children: globals.children,)],
-                              ),
-                              SizedBox(width: 20),
-                            ]),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          tablet: SingleChildScrollView(
-            reverse: true,
-            child: Container(
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Wrap(
-                                children: [
-                                  Column(children: globals.children,)],
-                              ),
-                              SizedBox(width: 20),
-                            ]),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          desktop: SingleChildScrollView(
-            reverse: true,
-            child: Column(children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'Assets/krowl_logo.png',
-                    scale: 2.0,
-                  ),
-                  SizedBox(
-                    width: 350,
-                  ),
-                  CustomTabBar(
-                    controller: tabController,
-                    tabs: contentViews.map((e) => e.tab).toList(),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 50,
-              ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 250.0,
-                  child: DefaultTextStyle(
-                    style: const TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Rubik',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                    child: AnimatedTextKit(
-                      totalRepeatCount: 5,
-                      animatedTexts: [
-                        WavyAnimatedText('Ask a question ?'),
-                      ],
-                      isRepeatingAnimation: true,
-                      onTap: () {
-                        print("Tap Event");
-                      },
-                    ),
-                  ),),
-                Container(
-                  width: 600,
-                  height: 600,
-                  color: globals.blue2,
-                )
-              ],
-            ),
-
-            ]),
-          ),
-        ));
+        body: CustomTabBar());
   }
 }
 
