590d5e36d50f4024a2759356590c57fab872efee
zogheibRawad <89975932+Toska003@users.noreply.github.com>
2022-01-20 12:23:42 +0200

    Revert "."
    This reverts commit 51439e27037b0fc3d57866745d2b2e31b582557f.

--- lib/widgets/PopUp/LoadingPopUp.dart
+++ lib/widgets/PopUp/LoadingPopUp.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 
-LoadingPopUp(context) {
+LoadingPopUp(context){
   showDialog<Image>(
     context: context,
     barrierDismissible: false,
     builder: (BuildContext context) => AlertDialog(
       content: Container(
-        height: 250,
-        width: 250,
-        child: Column(
-          children: [
-            Expanded(
-              child: Image(
-                image: AssetImage('Assets/krowl_logo.png'),
-              ),
-            ),
-            Text("Loading ..."),
-          ],
+        height: 150,
+        width: 150,
+        padding: const EdgeInsets.all(8.0),
+        child: Image(
+          image: AssetImage('Assets/krowl_logo.gif'),
         ),
       ),
     ),
