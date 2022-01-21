e7113918a9ca7c967ba9627e9e8418bd1519d457
zogheibRawad <89975932+Toska003@users.noreply.github.com>
2022-01-21 15:36:52 +0200

    Update globals.dart

--- lib/globals/globals.dart
+++ lib/globals/globals.dart
@@ -3,26 +3,26 @@ library flutterKrowl.globals;
 import 'package:flutter/material.dart';
 import 'package:flutter_app_backend/widgets/Chat/models/chat_users.dart';
 import 'package:flutter_app_backend/widgets/Forum/ReplyPage/RepliesWidget.dart';
-import 'package:flutter_app_backend/widgets/Library/CustomTable.dart';
 
 const String version = "v1.0";
 
 //Server Ip  (page[/my_api.dart])
 const String myIP = "https://kwikcode.net";
 //const String myIP = "http://127.0.0.1";
-const String jaasUrl ="https://KwikCode.net/krowlphp/jit/?table=";
+const String jaasUrl = "https://KwikCode.net/krowlphp/jit/?table=";
 
 //Colors
 Color white = Colors.white;
 Color blue1 = Colors.blue.shade900;
 Color blue2 = Colors.blue.shade50;
 
-
 //Errors
 const String error1 = "No Spaces Allowed.";
 const String error2_1 = "Your username must contain at least 8 characters.";
-const String error2_2 = "Your username can only contain lowercase and uppercase characters and special characters( _ .).";
-const String error2_3 = "Your password must contain at least 8 characters, 1 lowercase(a-z),1 uppercase(A-Z),1 numeric character(0-9) and 1 special character(* . ! @ # \$ % ^ & : , ? _ -).";
+const String error2_2 =
+    "Your username can only contain lowercase and uppercase characters and special characters( _ .).";
+const String error2_3 =
+    "Your password must contain at least 8 characters, 1 lowercase(a-z),1 uppercase(A-Z),1 numeric character(0-9) and 1 special character(* . ! @ # \$ % ^ & : , ? _ -).";
 const String error2_4 = "Your age must be greater than 17.";
 const String error2_5 = "It's not an email format.";
 const String error2_6 = "It's not a university email.";
@@ -39,9 +39,11 @@ const String errorEmptyContacts = "You doesn't have contacts yet";
 const String errorEmptyFriends = "You doesn't have friends yet";
 const String errorToken = "Token Error.";
 const String errorElse = "Failed to connect... Connection Problem.";
-const String errorException = "OOPs! Something went wrong. Try again in few seconds.";
+const String errorException =
+    "OOPs! Something went wrong. Try again in few seconds.";
 const String errorVersion = "New version available.";
-const String errorRememberMe= "Stay signed in?\n Do this to reduce the number of times you are asked to sign in.";
+const String errorRememberMe =
+    "Stay signed in?\n Do this to reduce the number of times you are asked to sign in.";
 //List of Universities
 List<dynamic> univercitiesName = [];
 //List of Majors
@@ -49,8 +51,8 @@ List<dynamic> degrees = [];
 
 //Registration  (page[/signup.dart /registration.dart /registration2.dart /registration3.dart])
 String? email = null;
-int?val2=null;  //for testing purpose
-String? val=null; //for testing purpose
+int? val2 = null; //for testing purpose
+String? val = null; //for testing purpose
 String? fName = null;
 String? lName = null;
 String? userName = null;
@@ -76,13 +78,12 @@ String? code4 = null;
 String? code5 = null;
 String? code6 = null;
 
-
 //login  (page[/login.dart /login2.dart])
-String? emailLogin ="";
+String? emailLogin = "";
 String? passwordLogin = "";
 
 //global
-String? currentPage = "Library";// Library Chat Forum Students Reminders
+String? currentPage = "Library"; // Library Chat Forum Students Reminders
 
 //Library
 //var tmpid = null;
@@ -97,10 +98,10 @@ var children2 = <ChatUsers>[];
 const apiKey = "z5j34vkctqrq";
 
 //Forum2
-String?dropdown2=null;
-String?question=null;
-String?context_question=null;
+String? dropdown2 = null;
+String? question = null;
+String? context_question = null;
 
 //Forum3
 var children3 = <Replies>[];
-String?reply_data=null;
+String? reply_data = null;
