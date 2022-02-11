2daa8d59483ae3b09c5e12a9dab4269fc94b2844
Clarazeidan <87119927+Clarazeidan@users.noreply.github.com>
2022-02-11 14:16:53 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.blue,
           ),
           //home: Intro(),
-          initialRoute: '/Test',
+          initialRoute: '/FirstPage',
           routes: {
             '/FirstPage': (context) => FirstPage(),
             '/intro_page': (context) => Intro(),
