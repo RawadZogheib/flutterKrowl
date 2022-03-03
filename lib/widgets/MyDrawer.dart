a99f39e5f83c881ca53fb84e15205d563324f3fb
zogheibRawad <rawadzogheib@gmail.com>
2022-03-03 16:12:42 +0200

    Update MyDrawer.dart

--- lib/widgets/MyDrawer.dart
+++ lib/widgets/MyDrawer.dart
@@ -1,127 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_backend/globals/globals.dart' as globals;
 import 'package:flutter_app_backend/hexColor/hexColor.dart';
-import 'package:flutter_app_backend/page/Responsive.dart';
 
 class myDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Drawer(
-        child: Material(
-          color: HexColor('#002756'), //globals.blue1,
-          child: SingleChildScrollView(
-            controller: ScrollController(),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 250,
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding: const EdgeInsets.all(40.0),
-                    child: Image.asset(
-                      'Assets/krowl_logo.gif',
-                      fit: BoxFit.cover,
+    return Drawer(
+      child: Material(
+        color: HexColor('#002756'), //globals.blue1,
+        child: SingleChildScrollView(
+          controller: ScrollController(),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Image.asset(
+                    'Assets/krowl_logo.gif',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 12),
+                    const SearchFieldDrawer(),
+                    const SizedBox(height: 12),
+                    MenuItem(
+                      text: 'Library',
+                      icon: Icons.menu_book,
+                      color: Colors.white,
+                      onClicked: () => selectedItem(context, 0),
                     ),
-                  ),
+                    const SizedBox(height: 5),
+                    MenuItem(
+                      text: 'Chat',
+                      icon: Icons.message_outlined,
+                      color: Colors.white,
+                      onClicked: () => selectedItem(context, 1),
+                    ),
+                    const SizedBox(height: 5),
+                    MenuItem(
+                      text: 'Forum',
+                      icon: Icons.forum_outlined,
+                      color: Colors.white,
+                      onClicked: () => selectedItem(context, 2),
+                    ),
+                    const SizedBox(height: 5),
+                    MenuItem(
+                      text: 'Students',
+                      icon: Icons.people,
+                      color: Colors.white,
+                      onClicked: () => selectedItem(context, 3),
+                    ),
+                    const SizedBox(height: 5),
+                    MenuItem(
+                      text: 'Reminders',
+                      icon: Icons.alarm,
+                      color: Colors.white,
+                      onClicked: () => selectedItem(context, 4),
+                    ),
+                    const SizedBox(height: 8),
+                    const Divider(color: Colors.white),
+                    const SizedBox(height: 8),
+                    MenuItem(
+                      text: 'Notifications',
+                      icon: Icons.notifications_outlined,
+                      color: globals.white,
+                      onClicked: () => selectedItem(context, 5),
+                    ),
+                    MenuItem(
+                      text: 'Settings',
+                      icon: Icons.settings,
+                      color: globals.white,
+                      onClicked: () => selectedItem(context, 6),
+                    ),
+                  ],
                 ),
-                Container(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 12),
-                      const SearchFieldDrawer(),
-                      const SizedBox(height: 12),
-                      MenuItem(
-                        text: 'Library',
-                        icon: Icons.menu_book,
-                          color: Colors.white,
-                        onClicked: () => selectedItem(context, 0),
-                      ),
-                      const SizedBox(height: 5),
-                      MenuItem(
-                        text: 'Chat',
-                        icon: Icons.message_outlined,
-                        color: Colors.white,
-                        onClicked: () => selectedItem(context, 1),
-                      ),
-                      const SizedBox(height: 5),
-                      MenuItem(
-                        text: 'Forum',
-                        icon: Icons.forum_outlined,
-                        color: Colors.white,
-                        onClicked: () => selectedItem(context, 2),
-                      ),
-                      const SizedBox(height: 5),
-                      MenuItem(
-                        text: 'Students',
-                        icon: Icons.people,
-                        color: Colors.white,
-                        onClicked: () => selectedItem(context, 3),
-                      ),
-                      const SizedBox(height: 5),
-                      MenuItem(
-                        text: 'Reminders',
-                        icon: Icons.alarm,
-                        color: Colors.white,
-                        onClicked: () => selectedItem(context, 4),
-                      ),
-                      const SizedBox(height: 8),
-                      const Divider(color: Colors.white),
-                      const SizedBox(height: 8),
-                      MenuItem(
-                        text: 'Notifications',
-                        icon: Icons.notifications_outlined,
-                        color: globals.white,
-                        onClicked: () => selectedItem(context, 5),
-                      ),
-                      MenuItem(
-                        text: 'Settings',
-                        icon: Icons.settings,
-                        color: globals.white,
-                        onClicked: () => selectedItem(context, 6),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 
   void selectedItem(BuildContext context, int index) {
     Navigator.of(context).pop();
     switch (index) {
-      case 0: // My Courses
-        // Navigator.of(context).push(MaterialPageRoute(
-        //   builder: (context) => const Scaffold(), // Page 1
-        // ));
+      case 0: // Library
+        if (globals.currentPage != 'Library') {
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/Library', (route) => false);
+        }
         break;
-      case 1: // Liked Courses
-        // Navigator.of(context).push(MaterialPageRoute(
-        //   builder: (context) => const Scaffold(), // Page 2
-        // ));
+      case 1: // Chat
+        if (globals.currentPage != 'Chat') {
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/MainChat', (route) => false);
+        }
         break;
-      case 2: // Liked Teachers
-        // Navigator.of(context).push(MaterialPageRoute(
-        //   builder: (context) => const Scaffold(), // Page 3
-        // ));
+      case 2: // Forum
+        if (globals.currentPage != 'Forum') {
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/Forum1', (route) => false);
+        }
         break;
-      case 3: // Updates
-        // Navigator.of(context).push(MaterialPageRoute(
-        //   builder: (context) => const Scaffold(), // Page 4
-        // ));
+      case 3: // Students
+        if (globals.currentPage != 'Students') {
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/Students1', (route) => false);
+        }
         break;
-      case 4: // Notifications
-        // Navigator.of(context).push(MaterialPageRoute(
-        //   builder: (context) => const Scaffold(), // Page 5
-        // ));
+      case 4: // Reminders
+        if (globals.currentPage != 'Reminders') {
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/Reminders', (route) => false);
+        }
         break;
-      case 5: // Settings
-        // Navigator.of(context).push(MaterialPageRoute(
-        //   builder: (context) => const Scaffold(), // Page 6
-        // ));
+      case 5: // Notifications
+        // if (globals.currentPage != 'Notifications') {
+        //   Navigator.pushNamedAndRemoveUntil(
+        //       context, '/Notifications', (route) => false);
+        // }
+        break;
+      case 6: // Settings
+        // if (globals.currentPage != 'Settings') {
+        //   Navigator.pushNamedAndRemoveUntil(
+        //       context, '/Settings', (route) => false);
+        // }
         break;
     }
   }
